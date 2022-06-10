f6465bf1d41f76600045ff802e34a0fa3259ae85
Alex Berger <alex@sternchen.club>
2022-06-10 20:54:39 +0200

    [boilerplate] Tidy up generated code
    - Add a license
    - Strip down README.md
    - Remove generated placeholder code

--- lib/formtool.ex
+++ lib/formtool.ex
@@ -2,17 +2,4 @@ defmodule Formtool do
   @moduledoc """
   Documentation for `Formtool`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Formtool.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/formtool_test.exs
+++ test/formtool_test.exs
@@ -1,8 +1,4 @@
 defmodule FormtoolTest do
   use ExUnit.Case
   doctest Formtool
-
-  test "greets the world" do
-    assert Formtool.hello() == :world
-  end
 end
