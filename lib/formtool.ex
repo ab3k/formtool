83a6d17f26f2c77f98b5fd538998c85a51731035
Alex Berger <alex@sternchen.club>
2022-06-10 20:31:28 +0200

    [boilerplate] Start a supervised project
    - mix new . --sup --module Formtool
    - mix test
    - mix run --no-halt
    - iex -S mix
    - iex> Formtool.hello()

---
+++ lib/formtool.ex
@@ -0,0 +1,18 @@
+defmodule Formtool do
+  @moduledoc """
+  Documentation for `Formtool`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Formtool.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/formtool_test.exs
@@ -0,0 +1,8 @@
+defmodule FormtoolTest do
+  use ExUnit.Case
+  doctest Formtool
+
+  test "greets the world" do
+    assert Formtool.hello() == :world
+  end
+end
