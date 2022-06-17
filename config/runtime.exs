994af87820cf6aed00a5a8933f12a0c573db1e3b
Alex Berger <alex@sternchen.club>
2022-06-17 12:32:36 +0200

    [dev] Refine VSCode setup
    - Add a devcontainer setup with docker-compose
    - Add runtime configuration for database connection for the devcontainer
      to use
    - Add convenience tasks

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,3 +5,20 @@ import Config
 # system starts, so it is typically used to load production configuration
 # and secrets from environment variables or elsewhere. Do not define
 # any compile-time configuration in here, as it won't be applied.
+
+# Change the DB host or the whole connection string. Handy for running in a
+# container or in production.
+cond do
+  database_host = System.get_env("DATABASE_HOST") ->
+    config :formtool, Formtool.Repo, hostname: database_host
+
+  database_url = System.get_env("DATABASE_URL") ->
+    config :formtool, Formtool.Repo, url: database_url
+
+  true ->
+    nil
+end
+
+if port = System.get_env("PORT") do
+  config :formtool, Formtool.Router, port: String.to_integer(port)
+end
